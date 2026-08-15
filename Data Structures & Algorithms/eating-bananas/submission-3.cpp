e9e8628509460835e7831f57cd1603db893ec830
Rishita Chaudhary <rishitachaudhary03@gmail.com>
2026-08-15 14:47:08 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1, r=*max_element(piles.begin(),piles.end());
+        int res=r;
+        while(l<=r){
+            int k=(l+r)/2;
+            long long totaltime=0;
+            for(int p :piles){
+                totaltime +=ceil(static_cast<double> (p)/k);
+            }
+                if(totaltime<=h){
+                    res=k;
+                    r=k-1;
+                }
+                else{
+                    l=k+1;
+                }
+            }
+        return res;
+    }
+};
