a3d5135248800803e8a040bddb669a10ccafc527
Rishita Chaudhary <rishitachaudhary03@gmail.com>
2026-08-18 10:58:21 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+    stack<int> stack;
+    std::stack<int> minstack;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stack.push(val);
+        val=min(val, minstack.empty()? val: minstack.top());
+        minstack.push(val);
+    }
+    
+    void pop() {
+        stack.pop();
+        minstack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minstack.top();
+    }
+};
